c3c060463dea8b5cdd4d84811a9b802857e9c851
kimmainsain <kmsid003@gmail.com>
2024-01-31 23:07:00 +0900

    :white_check_mark: Test : Jest 작동여부 확인

--- with-jest-app/__tests__/index.test.tsx
+++ with-jest-app/__tests__/index.test.tsx
@@ -1,17 +1,3 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from "@testing-library/react";
-import Home from "@/pages/home/index";
-
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
-
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
-    });
-
-    expect(heading).toBeInTheDocument();
-  });
+test('더하기 함수 테스트', () => {
+  expect(1 + 1).toBe(2);
 });
